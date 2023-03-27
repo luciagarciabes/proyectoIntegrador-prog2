827afa621f69a60647c00491a3b351df2efe603a
luciagarciabes <lgarciabes@udesa.edu.ar>
2023-03-27 20:32:41 -0300

    creo los archivos ejs, la carpeta de controladores, armo las rutas

--- proyecto/routes/index.js
+++ proyecto/routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//ruta de index
+router.get('/index', function (req,res) {
+  return res.render()
+  
+})
+
+//ruta de login
+router.get('login', function(req,res){
+  return res.render()
+})
+
+//ruta de registración
+router.get('/register', function(req,res){
+  return res.render()
+})
+
+//ruta de header logueado
+router.get("/headerLogueado", function(req,res){
+  return res.render()
+})
+
 module.exports = router;
--- proyecto/routes/users.js
+++ proyecto/routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//ruta de perfil
+router.get("/profile", function(req, res){
+  return res.render()
+})
+
+//ruta de editar perfil
+router.get("/editarPerfil", function(req,res){
+  return res.render()
+})
+
 module.exports = router;
