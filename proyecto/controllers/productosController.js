f2893d060eee4c5b954849c35453eb4b4b9712a9
luciagarciabes <lgarciabes@udesa.edu.ar>
2023-03-31 16:39:04 -0300

    agrego formularios

--- proyecto/controllers/productosController.js
+++ proyecto/controllers/productosController.js
@@ -1,5 +1,13 @@
 const productosControlador= {
-
+    detalleProducto: function(req, res) {
+        return res.render("product")  
+    },
+    searchResult: function(req,res){       
+        return res.render("search-results")
+    },
+    agregarProducto: function(req,res){
+        return res.render("product-add")
+    }
 }
 
 
--- proyecto/routes/productos.js
+++ proyecto/routes/productos.js
@@ -4,22 +4,15 @@ const productosControlador= require("../controllers/productosController")
 
 const datos= require("../data/datos")
 
-// ruta de lista de productos
-router.get("/detalleProducto",  function(req, res) {
-    return res.render("product")
-    
-})
+// ruta de detqlle de productos
+router.get("/detalleProducto", productosControlador.detalleProducto)
 
 
-//ruta de detalle de producto o search result
-router.get("/searchResult", function(req,res){       // hay que poner uno solo? o poner una ruta parametrica
-    return res.render("search-results")
-})
+//ruta de  search result
+router.get("/searchResult", productosControlador.searchResult)   // hay que poner uno solo? o poner una ruta parametrica
 
 //ruta de product add (solo tiene que renderizar una vista)
-router.get('/agregarProducto', function(req,res){
-    return res.render("product-add")
-})
+router.get('/agregarProducto', productosControlador.agregarProducto )
 
 
 
