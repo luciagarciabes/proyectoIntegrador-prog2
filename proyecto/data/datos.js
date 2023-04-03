e8b016131754b3d6689dbc8f21d60f9b0a3703ac
martinascarfoni <mscarfoni@udesa.edu.ar>
2023-04-03 10:12:19 -0300

    modulo de datos

--- proyecto/data/datos.js
+++ proyecto/data/datos.js
@@ -1,28 +1,140 @@
-const datos= {
-    usuarios: {
+const datos = {
+    usuario: {
+        email: 'clauditapaez@gmail.com',
+        usuario: 'Claudia Perez',
+        contraseña: 'claudita2023',
+        nacimiento: '31/03/1956',
+        documento: '10498320',
+        foto: ''        
     },
-    productos: [{nombre: "nombre Camistea" , precio:"$$$" },
-                {nombre: "nombre Camistea",precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" },
-                {nombre: "nombre Camistea" ,precio:"$$$" }],
-
-                
-    comentarios: [{nombreDeUsuario: "nombre", texto: "", imagrnDePerfil: ""},
-                  {nombreDeUsuario: "nombre", texto: "", imagrnDePerfil: ""},
-                  {nombreDeUsuario: "nombre", texto: "", imagrnDePerfil: ""},
-                  {nombreDeUsuario: "nombre", texto: "", imagrnDePerfil: ""}
-                  
-]
-
+    productos: [
+    {
+        nombre: "Chocotorta",
+        descripción: "La chocotorta se elabora con galletas de chocolate, crema de queso, dulce de leche y café.",
+        precio: 5000.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Cheesecake de arandanos",
+        descripción: "El cheesecake de arándanos se elabora con queso crema, azúcar, huevos, vainilla y arándanos frescos. La base de la tarta es de galletas trituradas mezcladas con mantequilla derretida.",
+        precio: 8500.00,
+        imagen: "imagen1.jpg", 
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Rogel",
+        descripción: "El rogel se elabora con harina, mantequilla, huevos, azúcar, dulce de leche y coco rallado.",
+        precio: 5500.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'     
+    },
+    {
+        nombre: "Torta de frutillas",
+        descripción: "La torta de frutillas es un postre elaborado con ingredientes básicos como harina, huevos, azúcar, leche, mantequilla y frutillas frescas.",
+        precio: 6500.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Torta Oreo",
+        descripción: "La torta Oreo es un postre delicioso elaborado con ingredientes simples como galletas Oreo, queso crema, azúcar y crema batida.",
+        precio: 8000.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Pasta frola de membrillo",
+        descripción: "La pasta frola de membrillo se elabora con ingredientes simples como harina, mantequilla, azúcar, huevos y dulce de membrillo.",
+        precio: 5000.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Torta de ricota",
+        descripción: "La torta de ricota es un postre italiano elaborado con queso ricota, huevos, azúcar, harina y esencia de vainilla.",
+        precio: 6500.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Marquise",
+        descripción: "La marquise es un postre de origen francés elaborado con chocolate, mantequilla, huevos y azúcar.",
+        precio: 7000.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Tiramisú",
+        descripción: "El tiramisú es un postre italiano que se elabora con ingredientes simples como queso mascarpone, bizcochos de soletilla, café, huevos y cacao en polvo.",
+        precio: 8500.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    },
+    {
+        nombre: "Torta de chocolate",
+        descripción: "La torta de chocolate es un postre clásico que se elabora con ingredientes simples como harina, azúcar, huevos, mantequilla, chocolate y cacao en polvo.",
+        precio: 7500.00,
+        imagen: "imagen1.jpg",
+        fechaDeCarga: '31/03/23'
+    }
+    ],
+    comentarios: [
+    {
+        usuario: "Juan Luanardo",
+        texto: "La torta que compré estaba deliciosa, la masa suave y esponjosa, y el relleno de chocolate era simplemente divino. Definitivamente volveré a comprar aquí.",
+        imagenPerfil: "perfil1.jpg"
+    },
+    {
+        usuario: "María Fajuardi",
+        texto: "No me gustó mucho la torta que compré aquí. La masa estaba muy seca y el relleno tenía un sabor extraño. Realmente esperaba algo mejor por el precio que pagué.",
+        imagenPerfil: "perfil2.jpg"
+    },
+    {
+        usuario: "Paola Pinpolón",
+        texto: "La torta fue simplemente espectacular. El sabor era delicioso, la masa estaba fresca y el relleno de frutas estaba muy bien distribuido. Sin duda alguna, recomiendo este lugar para comprar tortas.",
+        imagenPerfil: "perfil2.jpg"
+    },
+    {
+        usuario: "Miguel Echarri",
+        texto: "La torta superó todas mis expectativas. La masa estaba suave y esponjosa, el relleno era delicioso y la presentación era hermosa. Definitivamente volveré a comprar aquí.",
+        imagenPerfil: "perfil2.jpg"
+    },
+    {
+        usuario: "Beatriz García",
+        texto: "Muy rica y esponjosa.",
+        imagenPerfil: "perfil2.jpg"
+    },
+    {
+        usuario: "Juan Pablo Gutierrez",
+        texto: "Excelente servicio y envoltorio. La torta muy sabrosa.",
+        imagenPerfil: "perfil1.jpg"
+    },
+    {
+        usuario: "Marcela Suarez Mazzeo",
+        texto: "Exquisita. Volveré a comprar",
+        imagenPerfil: "perfil2.jpg"
+    },
+    {
+        usuario: "Cecilia Miranda",
+        texto: "Tal cual la esperaba. Rica y rendidora.",
+        imagenPerfil: "perfil2.jpg"
+    },
+    {
+        usuario: "Valentín Gramajo",
+        texto: "Espectacular el sabor y la consistencia.",
+        imagenPerfil: "perfil2.jpg"
+    },
+    {
+        usuario: "Valeria Pulpetta",
+        texto: "La torta estaba exquisita y la entrega súper rápida y prolija.",
+        imagenPerfil: "perfil2.jpg"
+    },
+    ]
 }
+
+
+
 
 
 
