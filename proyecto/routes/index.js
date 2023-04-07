56e162bb9ef5296a6d0c16b427911e3aa1442e79
luciagarciabes <lgarciabes@udesa.edu.ar>
2023-04-06 21:20:54 -0300

    elimino ruta de header logueado

--- proyecto/routes/index.js
+++ proyecto/routes/index.js
@@ -7,19 +7,11 @@ const indexControlador = require("../controllers/indexController")
 router.get('/', indexControlador.index);
 
 
-//ruta de index
-//router.get('/index', function (req,res) {
- // return res.render("")
-  
-//})
-
 //ruta de login
 router.get('/login', indexControlador.login)
 
 //ruta de registración
 router.get('/register', indexControlador.register)
 
-//ruta de header logueado
-router.get("/headerLogueado", indexControlador.headerLogueado)
 
 module.exports = router;
