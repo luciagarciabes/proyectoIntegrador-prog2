08dd9bdbd99d53fcbf43b89e8484dc0257eaafba
luciagarciabes <lgarciabes@udesa.edu.ar>
2023-04-11 08:43:40 -0300

    agrego fotos y cambio tamano

--- proyecto/controllers/usersController.js
+++ proyecto/controllers/usersController.js
@@ -2,7 +2,7 @@ const datos= require("../data/datos")
 
 const usersControlador= {
    profile: function(req, res){
-    return res.render("profile" ,{usuario: datos.usuario})
+    return res.render("profile" ,{usuario: datos.usuario, productos:datos.productos})
   },
   editarPerfil: function(req,res){
     return res.render("profile-edit", {usuario: datos.usuario})
