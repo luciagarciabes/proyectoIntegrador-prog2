27c04302ba8d935ac9eaa9f9419099e7afe61329
luciagarciabes <lgarciabes@udesa.edu.ar>
2023-05-22 12:30:15 -0300

    controlaor

--- proyecto/controllers/productosController.js
+++ proyecto/controllers/productosController.js
@@ -5,15 +5,19 @@ const Producto= db.Producto
 const productosControlador= {
     detalleProducto: function(req, res) { 
         let id= req.params.id
-        
+        //relaciones
 
         Producto.findByPk(id, {raw:true})
             .then(function (data) {
                 console.log(id);
                 console.log(data);
+                return res.render("product", {datos}) //le paso otros datos
+                
+            }). catch(function(error){
+                res.render()
             })
 
-        return res.render("product", {datos})  
+       
     },
     searchResult: function(req,res){       
         return res.render("search-results", {datos})
